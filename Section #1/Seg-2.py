99d753ed4865deafe3e74902f314a69946353c35
Bit77 <daren@markandtraci.com>
2025-10-18 14:26:39 -0500

    Update Seg-2.py

--- Section #1/Seg-2.py
+++ Section #1/Seg-2.py
@@ -1,1 +1,19 @@
+class PluginMeta(type):
+    registry = {}
 
+    def __new__(cls, name, bases, attrs):
+        new_class = super().__new__(cls, name, bases, attrs)
+        if name != "BasePlugin":
+            PluginMeta.registry[name] = new_class
+        return new_class
+
+class BasePlugin(metaclass=PluginMeta):
+    pass
+
+class PluginA(BasePlugin):
+    pass
+
+class PluginB(BasePlugin):
+    pass
+
+print("Registered plugins:", PluginMeta.registry)

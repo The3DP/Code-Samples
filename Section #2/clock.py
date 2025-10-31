b4f387a8cbb9309b9c26060f5dbc77e838877dd9
Bit77 <daren@markandtraci.com>
2025-10-30 20:45:21 -0500

    Add files via upload

---
+++ Section #2/clock.py
@@ -0,0 +1,8 @@
+### clock.py ###
+
+for hours in range(0,24,1);
+    # print("Just iterated hours")
+    for minutes in range (0, 60, 1):
+    #    print("just iterated minutes")
+        for seconds in range (0, 60, 1):
+            printf'(hours):(minutes):(seconds)')

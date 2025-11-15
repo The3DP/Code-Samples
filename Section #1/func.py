67086b2921001080b2e64fc4ab81444cd29a8439
Bit77 <daren@markandtraci.com>
2025-11-14 21:39:30 -0600

    Create func.py

---
+++ Section #1/func.py
@@ -0,0 +1,15 @@
+#functions
+
+
+varible1 = input ("enter a number")
+varible2 = input ("enter a second number")
+
+def add(varible1, varible2) :
+
+output = varible1 + varible2
+
+return output
+
+
+#call the function
+add (varible1, varible2)

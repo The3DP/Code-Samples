b49acd81725ca380ebfd1ef7224480c84a77594b
Bit77 <daren@markandtraci.com>
2026-02-02 14:33:09 -0600

    Add files via upload

---
+++ Morse Code Translator.py
@@ -0,0 +1,66 @@
+## Morse Code Converter ##
+## Date created: 1/30/2026
+## Goal: to create a self-working
+## morse code (to English) translator
+## using complete Python code.
+##====================================
+
+A = ['A is:', '.-']
+B = ['B is:',  '-...']
+C = ['C is:', '-.-.']
+D = ['D is:', '-..']
+E = ['E is:', '.']
+F = ['F is:', '..-.']
+G = ['G is:', '--.']
+H = ['H is:', '...']
+I = ['I is:', '..']
+J = ['J is:', '.---']
+K = ['K is:', '-.-']
+L = ['L is:', '.-..']
+M = ['M is:', '--']
+N = ['N is:', '-.']
+O = ['O is:', '---']
+P = ['P is:', '.--.']
+Q = ['Q is;', '--.-']
+R = ['R is:', '.-.']
+S = ['S is:', '..']
+T = ['T is:', '-']
+U = ['U is:', '..-']
+V = ['V is:', '...-']
+W = ['W is:', '.--']
+X = ['X is:', '-..-']
+Y = ['Y is:', '-.--']
+Z = ['Z is:', '--..']
+
+
+## FOR TUPLE TESTING ONLY
+## LEAVE COMMENTED OUT WHEN NOT TESTING
+## (KEY Commands: "^3" to comment out, "^4" to uncomment)
+     
+##print(A)
+##print(B)
+##print(C)
+##print(D)
+##print(E)
+##print(F)
+##print(G)
+##print(H)
+##print(I)
+##print(J)
+##print(K)
+##print(L)
+##print(M)
+##print(N)
+##print(O)
+##print(P)
+##print(Q)
+##print(R)
+##print(S)
+##print(T)
+##print(U)
+##print(V)
+##print(W)
+##print(X)
+##print(Y)
+##print(Z)
+

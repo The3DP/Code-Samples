5c91ac43edee4de481473b7ec906f52e782e4cdc
Bit77 <daren@markandtraci.com>
2026-02-09 18:30:53 -0600

    Add files via upload

---
+++ BAGP.py
@@ -0,0 +1,40 @@
+## Best (lichess.org) Atomic Games played
+## (BAGP)
+## Made: 1/4/2026
+## OBJ: Include a list of best played games.
+## Example Username: Matirx
+## We will include Matirx's 5 biggest victories.
+#################################################
+
+import time
+# Import time module
+
+### Variable Table ###
+username = "Matirx"
+number1 = 5
+opponent1 = 
+rating1 =
+opponent2 =
+rating2 =
+opponent3 =
+rating3
+opponent4 =
+rating4 =
+opponent5 =
+rating5 = 
+
+print("NOTE: Shows victories in this order: largest to smallest")
+time.sleep(0.2)
+print(" ...")
+time.sleep(0.3)
+print("Displaying", number1, "biggest victories for:", username)
+
+print("Victory #1 was against", opponent1, "Their rating was", rating1)
+
+print("Victory #2 was against", opponent2, "Their rating was", rating2)
+
+print("Victory #3 was against", opponent3, "Their rating was", rating3)
+
+print("Victory #4 was against", opponent4, "Their rating was", rating4)
+
+print("Victory #5 was against", opponent5, "Their rating was", rating5)

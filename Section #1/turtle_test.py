bf1d201499a7296f83a66632a380431db501149f
Bit77 <daren@markandtraci.com>
2026-02-09 19:54:11 -0600

    Update turtle_test.py

--- Section #1/turtle_test.py
+++ Section #1/turtle_test.py
@@ -9,6 +9,6 @@
 #  counter = counter+1
 #  t. end_fill()
 #  time. sleep(5)
-
+print(if_tried) #will move this up
 print("The commented code shown above is only as an example")
 if_tried = '''When uncommented, GitHub will shoot back the following error: ")

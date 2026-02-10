69c8b7236d88a3973f54635020384435aa57c14e
Bit77 <daren@markandtraci.com>
2026-02-09 18:38:44 -0600

    Comment out drone takeoff and video capture code
    Comment out the try block for takeoff and video capture.

--- Section #1/tello_vid.py
+++ Section #1/tello_vid.py
@@ -7,12 +7,12 @@ tello = Tello()
 tello.connect()
 tello.streamon()
 
-try:
-    # Take off and set an initial height
-    tello.takeoff()
-    tello.move_up(50)  # Adjust initial height as needed
-
-    while True:
-        # Capture video frame-by-frame
-        frame = tello.get_frame_read().frame
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+#try:
+#    # Take off and set an initial height
+#    tello.takeoff()
+#    tello.move_up(50)  # Adjust initial height as needed
+#
+#    while True:
+#        # Capture video frame-by-frame
+#        frame = tello.get_frame_read().frame
+#        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)

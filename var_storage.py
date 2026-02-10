16a4ed17feae5eaf9f2aad8ee5c9b03887a951ca
Bit77 <daren@markandtraci.com>
2026-02-10 09:42:47 -0600

    Create var_storage.py

---
+++ var_storage.py
@@ -0,0 +1,46 @@
+#===========================================================================
+# Demonstrates the use of simple variables in python
+# We can use so much as one character to represent a variable.
+# This file was added 2/10/2026 (Februrary 10th, 2026)
+# Data (A to I) collected via random calculations using Casio fx-300ES PLUS
+# Data (J to M) collected via random calculations using TI-30XIIS 
+#===========================================================================
+# Variable #1
+A = 362880
+
+# Variable #2
+B = 1.711224524
+
+# Variable #3
+C = 6.704425728
+
+# Variable #4
+D = 68.86885246
+
+# Variable #5
+E = 1.001563945
+
+# Variable #6
+F = 1.027995871
+
+# Variable #7
+G = 1.029850746
+
+# Variable #8
+H = 1.007933646
+
+# Variable #9
+I = '-1.711224524'
+
+# Variable #10
+J = 0.519098666
+
+# Variable #11
+K = '-2.00123169'
+
+# Variable #12
+L = '0.406349206'
+
+# Variable #13
+M = 
+

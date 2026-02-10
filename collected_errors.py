57de21cb6a76a413282cf6cbd83475d24b1641ca
Bit77 <daren@markandtraci.com>
2026-02-09 20:04:25 -0600

    Update collected_errors.py

--- collected_errors.py
+++ collected_errors.py
@@ -1,9 +1,17 @@
 #  < Here is a list ofrecent collected errors associated with the repository "Code-Samples" >
 print('''Here is a list ofrecent collected errors associated with the repository "Code-Samples":''')
-print(none)
 none = "No recent errors (< 1 month) detected so far :)"
-#error1 = #add if recent error detected
-#error2 = #add if recent error detected
-#error3 = #add if recent error detected
-#error4 = #add if recent error detected
-#error5 = #add if recent error detected
+#print(none)
+some = "One error (or more) was detected"
+print(some)
+  
+#  "Run # stop the build if there are Python syntax errors or undefined names
+#./Section #1/turtle_test.py:14:13: E999 SyntaxError: unterminated triple-quoted string literal (detected at line 14)
+#./collected_errors.py:3:7: F821 undefined name 'none'
+#print(none)
+#      ^
+#1     E999 SyntaxError: unterminated triple-quoted string literal (detected at line 14)
+#1     F821 undefined name 'none'
+#2
+#Most recent error^
+# Print statement commented out if not used

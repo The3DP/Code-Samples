9bc340d7bba44e8614382f79f480cfbb30b3f9e8
Bit77 <daren@markandtraci.com>
2026-02-09 19:13:47 -0600

    Update turtle_test.py

--- Section #1/turtle_test.py
+++ Section #1/turtle_test.py
@@ -4,8 +4,8 @@ t. color("blue")
 t. begin_fill()
 counter=0
 while counter < 4:
-t. forward(100)
-t. left(90)
-counter = counter+1
-t. end_fill()
-time. sleep(5)
+  t. forward(100)
+  t. left(90)
+  counter = counter+1
+  t. end_fill()
+  time. sleep(5)
